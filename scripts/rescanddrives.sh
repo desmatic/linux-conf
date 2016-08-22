36c86d3b91daa86c4d2847bd3eb1a9a493e90375
dejones <dejones@alertlogic.com>
2016-08-22 11:15:30 +0100

    update rescan script

--- scripts/rescanddrives.sh
+++ scripts/rescanddrives.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 find /sys/class/scsi_host/*/ -name 'scan' -exec echo "- - -" >  '{}' \;
 find /sys/class/scsi_device/*/device/ -name 'rescan' -exec echo 1 > '{}' \;
+find /sys/block/sd*/device/ -name 'rescan' -exec echo 1 > '{}' \;
